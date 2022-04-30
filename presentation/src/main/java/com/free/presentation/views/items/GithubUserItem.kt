333a17906d76f0b3a581aec4b3614b9de5d0add2
yoshimuratakuma0 <outofbody21@gmail.com>
2022-05-01 01:37:11 +0900

    :lipstick: make icon rounded

--- presentation/src/main/java/com/free/presentation/views/items/GithubUserItem.kt
+++ presentation/src/main/java/com/free/presentation/views/items/GithubUserItem.kt
@@ -7,18 +7,37 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.transform.RoundedCornersTransformation
 import com.free.domain.entities.User
 
 
 @Composable
 fun GithubUserItem(user: User) {
+    val r = 32
     Row {
         Image(
-            painter = rememberAsyncImagePainter(user.avatarUrl),
+            painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(LocalContext.current).data(data = user.avatarUrl)
+                    .apply(block = {
+                        transformations(
+                            with(LocalDensity.current) {
+                                RoundedCornersTransformation(
+                                    topLeft = r.dp.toPx(),
+                                    topRight = r.dp.toPx(),
+                                    bottomLeft = r.dp.toPx(),
+                                    bottomRight = r.dp.toPx(),
+                                )
+                            }
+                        )
+                    }).build()
+            ),
             contentDescription = null,
-            modifier = Modifier.size(128.dp)
+            modifier = Modifier.size((2 * r).dp)
         )
         Text(text = user.username, color = Color.Gray)
     }
