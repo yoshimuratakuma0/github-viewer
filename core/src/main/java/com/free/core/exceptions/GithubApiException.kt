5d364f576b604e2f31de09d7664f2d2a87c6bca0
yoshimuratakuma0 <outofbody21@gmail.com>
2022-06-11 01:40:47 +0900

    :recycle: library

--- core/src/main/java/com/free/core/exceptions/GithubApiException.kt
+++ core/src/main/java/com/free/core/exceptions/GithubApiException.kt
@@ -1,25 +1,25 @@
 package com.free.core.exceptions
 
-sealed class GithubApiException(message: String, statusCode: Int) : Exception(message) {
-    class NotModifiedException(message: String, statusCode: Int) :
-        GithubApiException(message, statusCode)
+sealed class GithubApiException(message: String) : Exception(message) {
+    class NotModifiedException(message: String) :
+        GithubApiException(message)
 
-    class UnauthenticatedException(message: String, statusCode: Int) :
-        GithubApiException(message, statusCode)
+    class UnauthenticatedException(message: String) :
+        GithubApiException(message)
 
-    class ForbiddenException(message: String, statusCode: Int) :
-        GithubApiException(message, statusCode)
+    class ForbiddenException(message: String) :
+        GithubApiException(message)
 
-    class UnknownException(message: String, statusCode: Int) :
-        GithubApiException(message, statusCode)
+    class UnknownException(message: String) :
+        GithubApiException(message)
 
     companion object {
         fun fromStatusCode(statusCode: Int): GithubApiException {
             return when (statusCode) {
-                304 -> NotModifiedException("not modified exception", statusCode)
-                401 -> UnauthenticatedException("unauthenticated exception", statusCode)
-                403 -> ForbiddenException("forbidden exception", statusCode)
-                else -> UnknownException("unknown exception", statusCode)
+                304 -> NotModifiedException("not modified exception")
+                401 -> UnauthenticatedException("unauthenticated exception")
+                403 -> ForbiddenException("forbidden exception")
+                else -> UnknownException("unknown exception")
             }
         }
     }
