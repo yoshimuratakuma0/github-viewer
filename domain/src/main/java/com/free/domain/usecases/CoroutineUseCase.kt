5727039748e79f0360806ff31a3a90543f617630
吉村琢磨 <yoshimuratakuma@yoshimuratakumanoMac-mini.local>
2023-02-18 00:39:50 +0900

    :pick: CorountineUseCase#

---
+++ domain/src/main/java/com/free/domain/usecases/CoroutineUseCase.kt
@@ -0,0 +1,23 @@
+package com.free.domain.usecases
+
+import com.free.domain.Result
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+abstract class CoroutineUseCase<P, R>(
+    private val dispatcher: CoroutineDispatcher
+) {
+    suspend operator fun invoke(params: P): Result<R> {
+        return try {
+            withContext(dispatcher) {
+                execute(params).let {
+                    Result.Success(it)
+                }
+            }
+        } catch (exception: Exception) {
+            Result.Error(exception)
+        }
+    }
+
+    internal abstract suspend fun execute(params: P): R
+}
