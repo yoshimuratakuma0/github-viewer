4d04d8a6b507ba9cabee01e8839df3278e48b9cd
yoshimuratakuma0 <outofbody20@gmail.com>
2024-06-01 15:09:34 +0900

    :recycle: Migrate domain:build.gradle to kts

---
+++ domain/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+}
