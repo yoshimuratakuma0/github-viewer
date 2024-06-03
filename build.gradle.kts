ff82bd7327421e569e5b147a0f3cbdf8df6cb41c
yoshimuratakuma0 <outofbody20@gmail.com>
2024-06-03 23:47:18 +0900

    :recycle: Define version catalog for plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,11 @@ buildscript {
 }
 
 plugins {
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt.android) apply false
 }
 
 tasks.register("clean", Delete::class) {
