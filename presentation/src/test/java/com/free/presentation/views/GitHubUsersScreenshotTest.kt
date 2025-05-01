4c1bb63de6f2ffca17aa03c65c248f7e082b1bce
yoshimuratakuma0 <outofbody20@gmail.com>
2025-05-01 23:38:28 +0900

    Remove colors from texts

--- presentation/src/test/java/com/free/presentation/views/GitHubUsersScreenshotTest.kt
+++ presentation/src/test/java/com/free/presentation/views/GitHubUsersScreenshotTest.kt
@@ -19,7 +19,7 @@ class GitHubUsersScreenshotTest {
     val composeRule = createComposeRule()
 
     @Test
-    fun test() {
+    fun test_success() {
         composeRule.setContent {
             GithubUsersStatelessScreen(
                 listState = rememberLazyListState(),
@@ -41,6 +41,63 @@ class GitHubUsersScreenshotTest {
                         avatarUrl = "https://avatars.githubusercontent.com/u/1?v=4"
                     ),
                 ),
+                fetchMore = {},
+                onClick = {},
+                onFollowing = {},
+                onFollowers = {},
+            )
+        }
+
+        composeRule
+            .onRoot()
+            .captureRoboImage()
+    }
+
+    @Test
+    fun test_loading() {
+        composeRule.setContent {
+            GithubUsersStatelessScreen(
+                listState = rememberLazyListState(),
+                uiState = GitHubUsersUiState.Loading,
+                users = emptyList(),
+                fetchMore = {},
+                onClick = {},
+                onFollowing = {},
+                onFollowers = {},
+            )
+        }
+
+        composeRule
+            .onRoot()
+            .captureRoboImage()
+    }
+
+    @Test
+    fun test_error() {
+        composeRule.setContent {
+            GithubUsersStatelessScreen(
+                listState = rememberLazyListState(),
+                uiState = GitHubUsersUiState.Error(Exception()),
+                users = emptyList(),
+                fetchMore = {},
+                onClick = {},
+                onFollowing = {},
+                onFollowers = {},
+            )
+        }
+
+        composeRule
+            .onRoot()
+            .captureRoboImage()
+    }
+
+    @Test
+    fun test_no_data() {
+        composeRule.setContent {
+            GithubUsersStatelessScreen(
+                listState = rememberLazyListState(),
+                uiState = GitHubUsersUiState.NoData,
+                users = emptyList(),
                 fetchMore = {},
                 onClick = {},
                 onFollowing = {},
