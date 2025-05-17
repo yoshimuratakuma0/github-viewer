3d44d6d8e7670ebc51a8b70d4df4ce72ca57e7c9
yoshimuratakuma0 <outofbody20@gmail.com>
2025-05-17 22:34:33 +0900

    Add previews in design-system

--- design-system/src/main/java/com/design_system/components/Button.kt
+++ design-system/src/main/java/com/design_system/components/Button.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Button(
@@ -30,4 +31,12 @@ fun Button(
         interactionSource = interactionSource,
         content = content
     )
+}
+
+@Preview
+@Composable
+fun ButtonPreview() {
+    Button(onClick = { }) {
+        Text(text = "Button")
+    }
 }
--- design-system/src/main/java/com/design_system/components/Card.kt
+++ design-system/src/main/java/com/design_system/components/Card.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Card(
@@ -20,4 +21,12 @@ fun Card(
         border = border,
         content = content,
     )
+}
+
+@Preview
+@Composable
+fun CardPreview() {
+    Card {
+        Text(text = "Card")
+    }
 }
