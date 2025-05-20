aab9cdf47bf2cbca595f89e198b4509723174ff8
yoshimuratakuma0 <outofbody20@gmail.com>
2025-05-20 19:22:21 +0900

    Introduce HeaderInterceptor

--- data/src/main/java/com/free/data/datasources/GitHubApi.kt
+++ data/src/main/java/com/free/data/datasources/GitHubApi.kt
@@ -4,26 +4,22 @@ import com.free.data.models.UserDetailResponse
 import com.free.data.models.UserResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GitHubApi {
 
-    @Headers("Accept: application/vnd.github.v3+json")
     @GET("users")
     suspend fun users(
         @Query("since") since: Int?,
         @Query("per_page") perPage: Int
     ): Response<List<UserResponse>>
 
-    @Headers("Accept: application/vnd.github.v3+json")
     @GET("users/{username}")
     suspend fun userDetail(
         @Path("username") username: String
     ): Response<UserDetailResponse>
 
-    @Headers("Accept: application/vnd.github.v3+json")
     @GET("users/{username}/followers")
     suspend fun followers(
         @Path("username") username: String,
@@ -31,7 +27,6 @@ interface GitHubApi {
         @Query("per_page") perPage: Int,
     ): Response<List<UserResponse>>
 
-    @Headers("Accept: application/vnd.github.v3+json")
     @GET("users/{username}/following")
     suspend fun following(
         @Path("username") username: String,
