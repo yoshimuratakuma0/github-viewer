5517de36171a25245123a99ff04fad3dc467c72c
yoshimuratakuma0 <outofbody20@gmail.com>
2025-05-28 03:18:27 +0900

    Revert ":rotating_light:"
    This reverts commit 339074b815a39dfeba2b8dadbd41f812204ac1ef.

--- feature-user/src/main/java/com/free/feature_user/items/UserItem.kt
+++ feature-user/src/main/java/com/free/feature_user/items/UserItem.kt
@@ -104,7 +104,7 @@ fun UserItem(
 @Composable
 fun PreviewUserItem() {
     UserItem(
-        name = "iey",
+        name = "ivey",
         iconUrl = "https://avatars.githubusercontent.com/u/6?v=4",
         onClick = {},
         onFollowing = {},
